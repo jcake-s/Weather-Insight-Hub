5d8448307a5a981ef23d0c543675914ced48c4b0
J Cajes <joscajes@gmail.com>
2026-06-21 15:24:35 +0800

    Added new features and restructured the whole thing

--- script.js
+++ script.js
@@ -2,16 +2,6 @@ const cityInput = document.getElementById('cityInput');
 const searchBtn = document.getElementById('searchBtn');
 const WeatherResult = document.getElementById('WeatherResult');
 
-searchBtn.addEventListener('click', function() {
-    const cityName = cityInput.value;
-
-    if (cityName !== "") {
-        getWeather(cityName);
-    } else {
-        WeatherResult.innerHTML = "<p>Please enter a city name.</p>";
-    }
-});
-
 function getWeather(city) {
     const apiKey = '28121f82d1c96932a75c6c22dd81946f';
     const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
@@ -23,8 +13,31 @@ function getWeather(city) {
                 <h2>${data.name}</h2>
                 <p>Temperature: ${data.main.temp}°C</p>
                 <p>Condition: ${data.weather[0].description}</p>`;
+            
+            const weatherCondition = data.weather[0].main;
+            document.body.className = weatherCondition.toLowerCase();
         })
         .catch(error => {
-            WeatherResult.innerHTML = "<p>City not found. Please pick a goddamn existing country!</p>";
+            WeatherResult.innerHTML = "<p>City not found. Please pick a goddamn existing place!</p>";
         });
 }
+
+searchBtn.addEventListener('click', function() {
+    const cityName = cityInput.value;
+
+    if (cityName !== "") {
+        getWeather(cityName);
+    } else {
+        WeatherResult.innerHTML = "<p>Please enter a city name.</p>";
+    }
+});
+
+cityInput.addEventListener('keypress', function(event) {
+    if (event.key === 'Enter') {
+        event.preventDefault();
+        const cityName = cityInput.value;
+        if (cityName !== "") {
+            getWeather(cityName);
+        }
+    }
+});
