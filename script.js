bfbe52653501e85e144b882455f0c6f0ee31e03f
J Cajes <joscajes@gmail.com>
2026-06-21 19:43:24 +0800

    Added loading effects

--- script.js
+++ script.js
@@ -3,12 +3,19 @@ const searchBtn = document.getElementById('searchBtn');
 const WeatherResult = document.getElementById('WeatherResult');
 
 function getWeather(city) {
+    const loader = document.getElementById('loader');
+    const WeatherResult = document.getElementById('WeatherResult');
+
+    loader.style.display = 'block';
+    WeatherResult.innerHTML = '';
+    
     const apiKey = '28121f82d1c96932a75c6c22dd81946f';
     const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
 
     fetch(apiUrl)
         .then(response => response.json())
         .then(data => {
+            loader.style.display = 'none';
             WeatherResult.innerHTML = `
                 <h2>${data.name}</h2>
                 <p>Temperature: ${data.main.temp}°C</p>
@@ -18,6 +25,7 @@ function getWeather(city) {
             document.body.className = weatherCondition.toLowerCase();
         })
         .catch(error => {
+            loader.style.display = 'none';
             WeatherResult.innerHTML = "<p>City not found. Please pick a goddamn existing place!</p>";
         });
 }
